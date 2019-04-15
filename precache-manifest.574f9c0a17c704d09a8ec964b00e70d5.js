b0bafca3cc6a26f00bf25bca43c28f7ca3037da2
Rick Brown <ricky.brown.00@gmail.com>
2019-04-15 14:07:11 +0100

    Updates

---
+++ precache-manifest.574f9c0a17c704d09a8ec964b00e70d5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6054ea27a3c3b65cf91a",
+    "url": "/RickBr0wn/rock-paper-scissors/static/css/main.2d76969c.chunk.css"
+  },
+  {
+    "revision": "6054ea27a3c3b65cf91a",
+    "url": "/RickBr0wn/rock-paper-scissors/static/js/main.6054ea27.chunk.js"
+  },
+  {
+    "revision": "0cd6058fc3c24877ba89",
+    "url": "/RickBr0wn/rock-paper-scissors/static/js/1.0cd6058f.chunk.js"
+  },
+  {
+    "revision": "6ada889ebeea1e4254d7",
+    "url": "/RickBr0wn/rock-paper-scissors/static/js/runtime~main.6ada889e.js"
+  },
+  {
+    "revision": "46e622ca1659c5ccf5e54b56b9be4eca",
+    "url": "/RickBr0wn/rock-paper-scissors/index.html"
+  }
+];
--- src/Components/GameCounter.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-const GameCounter = ({ seconds }) => {
-  return <div className="game-counter">{seconds}</div>
-}
-
-export default GameCounter
